f89a6d30d92a42290839c702591b2d4fd82e2912
castrintt <igordc38@gmail.com>
2022-03-20 10:48:06 -0300

    alterado praticamente tudo

--- models/Job.js
+++ models/Job.js
@@ -1,31 +1,25 @@
-const Sequelize = require('sequelize')
-
-const db = require('../db/connection') 
-
-//criando banco
+const Sequelize = require('sequelize');
+const db = require('../db/connection');
 
 const Job = db.define('job', {
-    title : {
-        type : Sequelize.STRING,
-    },
-    description : {
-        type : Sequelize.STRING,
-    },
-    salary : {
-        type : Sequelize.STRING,
-    },
-    company : {
-        type : Sequelize.STRING,
-    },
-    email : {
-        type : Sequelize.STRING,
-    },
-    new_job: {
-        type : Sequelize.INTEGER,
-    }
-    
-})
-
-//exportando
+  title: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  salary: {
+    type: Sequelize.STRING,
+  },
+  company: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  new_job: {
+    type: Sequelize.INTEGER,
+  }
+});
 
 module.exports = Job
